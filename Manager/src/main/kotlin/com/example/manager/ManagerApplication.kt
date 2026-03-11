75f4ba1b842f2bd424b5bc6c43aecb98abe959e3
Just <grivten@yandex.ru>
2026-03-11 11:45:51 +0700

    init manager3

--- Manager/src/main/kotlin/com/example/manager/ManagerApplication.kt
+++ Manager/src/main/kotlin/com/example/manager/ManagerApplication.kt
@@ -1,4 +1,4 @@
-package com.example.Manager
+package com.example.manager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- Manager/src/test/kotlin/com/example/manager/ManagerApplicationTests.kt
+++ Manager/src/test/kotlin/com/example/manager/ManagerApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.Manager
+package com.example.manager
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
